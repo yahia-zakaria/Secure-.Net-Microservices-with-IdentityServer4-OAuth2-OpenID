b74e3f1302873e052ddd64611b9a3abfccb74729
yahia-zakaria <yahiazakaria91@hotmail.com>
2022-07-12 23:28:41 +0300

    Applying Retry pattern in Database Migration and Seeding in Discount gRPC service

--- Discount/src/Discount.Grpc/Extensions/IHostExtensions.cs
+++ Discount/src/Discount.Grpc/Extensions/IHostExtensions.cs
@@ -4,27 +4,47 @@ using Microsoft.Extensions.Logging;
 using System.Data;
 using Dapper;
 using System;
+using Polly;
+using Npgsql;
+using Serilog;
 
 namespace Discount.Grpc.Extensions
 {
     public static class IHostExtensions
     {
 
-        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        public static IHost MigrateDatabase<TContext>(this IHost host)
         {
-            var retryForAvailability = retry.Value;
             using var scope = host.Services.CreateScope();
             var serviceProvider = scope.ServiceProvider;
 
-            var logger = serviceProvider.GetRequiredService<ILogger<TContext>>(); 
+            var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
             var conn = serviceProvider.GetRequiredService<IDbConnection>();
 
+            //implement retry pattern
+            var retry = Policy
+        .Handle<NpgsqlException>()
+        .WaitAndRetry(
+        retryCount: 5,
+        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        onRetry: (exception, timespan, context) =>
+        {
+            Log.Error($"Retry after {timespan.TotalMilliseconds} seconds due to {exception}");
+        });
+
+            retry.Execute(() => ExecutingMigration(logger, conn));
+
+            return host;
+        }
+
+        private static void ExecutingMigration<TContext>(ILogger<TContext> logger, IDbConnection conn)
+        {
             conn.Open();
             logger.LogInformation("Start database migration....");
             var tableExists = conn.ExecuteScalar<bool>("Select EXISTS(SELECT FROM pg_tables WHERE schemaname=@schemaName AND tablename=@TableName)",
                 new { schemaName = "public", tableName = "coupon" });
 
-            if(!tableExists)
+            if (!tableExists)
             {
                 try
                 {
@@ -34,31 +54,23 @@ namespace Discount.Grpc.Extensions
                               "Description text," +
                               "Amount int" +
                               ")";
-                     conn.Execute(ddl);
+                    conn.Execute(ddl);
 
                     var sql = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES(@productName, @desc, @amount)";
-                    conn.Execute(sql, new { productName  = "IPhone X", desc = "IPhone Discount", amount = 150});
-                    conn.Execute(sql, new { productName  = "Samsung 10", desc = "Samsung Discount", amount = 100});
+                    conn.Execute(sql, new { productName = "IPhone X", desc = "IPhone Discount", amount = 150 });
+                    conn.Execute(sql, new { productName = "Samsung 10", desc = "Samsung Discount", amount = 100 });
 
                     logger.LogInformation("Finishing database migration....");
                 }
-                catch (Exception)
+                catch (NpgsqlException ex)
                 {
-                    logger.LogError("Error while migrating database....");
-                    if (retryForAvailability < 50)
-                    {
-                        retryForAvailability++;
-                        System.Threading.Thread.Sleep(2000);
-                        MigrateDatabase<TContext>(host, retryForAvailability);
-                    }
+                    logger.LogError("Error while migrating database due to {ex}", ex);
                 }
                 finally
                 {
                     conn.Close();
                 }
             }
-
-            return host;
         }
     }
 }
