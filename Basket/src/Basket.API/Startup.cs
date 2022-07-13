8cd7d3f6e5354346143e542c11bb656f4eab9fd1
yahia-zakaria <yahiazakaria91@hotmail.com>
2022-07-14 02:49:17 +0300

    Adding Health Checks to Basket API

--- Basket/src/Basket.API/Startup.cs
+++ Basket/src/Basket.API/Startup.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Basket.API.GrpcServices;
 using Basket.API.Repository;
+using HealthChecks.UI.Client;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
@@ -52,6 +55,11 @@ namespace Basket.API
 
             services.AddAutoMapper(typeof(Startup));
 
+
+            services.AddHealthChecks()
+                    .AddRedis(Configuration["CachingSettings:ConnectionString"], "Redis Health", HealthStatus.Unhealthy)
+                    .AddRabbitMQ(Configuration["EventBusSettings:HostAddress"], null, "RabbitMQ Health", HealthStatus.Unhealthy);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +79,11 @@ namespace Basket.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
